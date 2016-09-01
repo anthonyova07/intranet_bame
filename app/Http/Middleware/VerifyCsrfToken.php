08b74ec334315056b9b2df10cbfe6e9ab020323e
Jeison Peguero <j.peguero@bancamerica.com.do>
2016-09-01 11:00:22 -0400

    mejora seguridad para capturar la exception del token de seguridad

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace Bame\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
@@ -14,4 +15,18 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    public function handle($request, Closure $next)
+    {
+        if (
+            $this->isReading($request) ||
+            $this->runningUnitTests() ||
+            $this->shouldPassThrough($request) ||
+            $this->tokensMatch($request)
+        ) {
+            return $this->addCookieToResponse($request, $next($request));
+        }
+
+        return back()->with('error', 'El TOKEN de seguridad no pudo ser validado. Favor refrescar la pagina.');
+    }
 }
