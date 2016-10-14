72535b0a29d27efe8c53a21edaef824b5e5ff9b9
Jeison Peguero <j.peguero@bancamerica.com.do>
2016-10-14 10:19:26 -0400

    creacion del permiso para el menu rrhh

--- resources/views/marketing/event/index.blade.php
+++ resources/views/marketing/event/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('page_title', 'Mantenimiento de Eventos')
 
-@if (can_not_do('marketing_event'))
+@if (can_not_do('marketing_event') && can_not_do('human_resources_event'))
     @section('contents')
         @include('layouts.partials.access_denied')
     @endsection
