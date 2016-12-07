c3c3be3196d8f0f3bd31dbf2a5b520c0e6abf74f
Jeison Peguero <j.peguero@bancamerica.com.do>
2016-12-07 12:21:04 -0400

    quitar alert cuando no soporta notificaciones el navegador

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -5,7 +5,7 @@ function desktop_noty(title, body, url) {
     }
     // Let's check if the browser supports notifications
     if (!("Notification" in window)) {
-        alert("This browser does not support desktop notification");
+        console.log("This browser does not support desktop notification");
     }
 
     // Let's check whether notification permissions have already been granted
