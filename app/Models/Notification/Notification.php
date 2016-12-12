8bd716aab22c5615ef7630c198d0ef16d767bb5d
Jeison Peguero <j.peguero@bancamerica.com.do>
2016-12-12 10:41:18 -0400

    notifica usuarios por permiso

--- app/Models/Notification/Notification.php
+++ app/Models/Notification/Notification.php
@@ -52,6 +52,21 @@ class Notification
         $noti->save();
     }
 
+    public static function notifyUsersByPermission($permission, $title, $body, $url = '')
+    {
+        $sub_menu = \Bame\Models\Security\SubMenu::where('sub_coduni', $permission)->first();
+
+        if ($sub_menu) {
+            $users = \Bame\Models\Security\Access::where('acc_codmen', $sub_menu->sub_codmen)
+                ->where('acc_submen', $sub_menu->sub_codigo)->get();
+
+            foreach ($users as $user) {
+                self::notify($title, $body, $url, $user->acc_user);
+            }
+        }
+
+    }
+
     public function create($titulo, $texto, $url = '')
     {
         $notificacion = new \stdClass;
