610ae82309af35fbf8e1639e122b45bb80244f0f
Jeison Peguero Castro <jpeguerocastro@gmail.com>
2017-03-08 17:21:04 -0400

    reportes de recursos humanos compleatado

--- app/Http/Controllers/HumanResource/Queries/QueryController.php
+++ app/Http/Controllers/HumanResource/Queries/QueryController.php
@@ -11,28 +11,34 @@ use DB;
 
 class QueryController extends Controller {
 
+    public function index(Request $request)
+    {
+        return view('human_resources.queries.index');
+    }
+
     //Reporte Cuentas tipo H201/H202
     public function reporte_cuentas()
     {
-        $results = DB::connection('ibs')->select("select
-            trim(acmopd) dia_apertura,
-            trim(acmopm) mes_apertura,
-            trim(acmopy) anio_apertura,
-            trim(acmbrn) sucursal,
-            trim(acmacc) numero_cuenta,
-            trim(acmcun) codigo_cliente,
-            trim(cusna1) nombre_cliente,
-            trim(acmaty) producto,
-            trim(acmpro) tipo,
-            trim(acmast) estado,
-            trim(acmccy) moneda,
-            trim(acmgbl) *-1 saldo_libro,
-            trim(acmiac) interes_por_pagar,
-            trim(acmipl) interes_cobrado_anio,
-            case when acmacd = '04' then 'Ahorro' else  'Corriente' end as tipo
-            from acmst, cumst
-            where cuscun = acmcun and
-            acmpro in('H201','H202')");
+        $results = DB::connection('ibs')
+            ->select("SELECT
+                TRIM(ACMOPD) DIA_APERTURA,
+                TRIM(ACMOPM) MES_APERTURA,
+                TRIM(ACMOPY) ANIO_APERTURA,
+                TRIM(ACMBRN) SUCURSAL,
+                TRIM(ACMACC) NUMERO_CUENTA,
+                TRIM(ACMCUN) CODIGO_CLIENTE,
+                TRIM(CUSNA1) NOMBRE_CLIENTE,
+                TRIM(ACMATY) PRODUCTO,
+                TRIM(ACMPRO) TIPO,
+                TRIM(ACMAST) ESTADO,
+                TRIM(ACMCCY) MONEDA,
+                TRIM(ACMGBL) *-1 SALDO_LIBRO,
+                TRIM(ACMIAC) INTERES_POR_PAGAR,
+                TRIM(ACMIPL) INTERES_COBRADO_ANIO,
+                CASE WHEN ACMACD = '04' THEN 'AHORRO' ELSE  'CORRIENTE' END AS TIPO
+                FROM ACMST, CUMST
+                WHERE CUSCUN = ACMCUN AND
+                ACMPRO IN('H201','H202')");
 
         return view('layouts.queries.excel', compact('results'));
     }
@@ -40,11 +46,89 @@ class QueryController extends Controller {
     //Reporte Vinculados por gestión
     public function reporte_vinculados_gestion()
     {
-        $results = DB::connection('ibs')->select("select cuscun codigocliente,
-            trim(cusna1) nombre,
-            trim(cusuc1) vinculacion
-            from cumst
-            where cusuc1 in('G1','G2')");
+        $results = DB::connection('ibs')
+            ->select("SELECT CUSCUN CODIGOCLIENTE,
+                TRIM(CUSNA1) NOMBRE,
+                TRIM(CUSUC1) VINCULACION
+                FROM CUMST
+                WHERE CUSUC1 IN('G1','G2')");
+
+        return view('layouts.queries.excel', compact('results'));
+    }
+
+    //Reporte Oficial asignado (244: Bianca / 187: Victoria)
+    public function reporte_oficial_asignado()
+    {
+        $results = DB::connection('ibs')
+            ->select("SELECT
+                TRIM(CUSCUN) CODIGO_CLIENTE,
+                TRIM(C.CUSNA1) NOMBRE,
+                TRIM(C.CUSOFC) CODIGO_OFICIAL_PRINCIPAL,
+                TRIM((SELECT
+                    CNODSC
+                    FROM CNOFC
+                    WHERE CNOCFL = '15'
+                    AND CNORCD = C.CUSOFC)) AS NOMBRE_OFICIAL_PRINCIPAL,
+                TRIM(C.CUSOF2) CODIGO_OFICIAL_SEGUNDO,
+                TRIM((SELECT
+                    CNODSC
+                    FROM CNOFC
+                    WHERE CNOCFL = '15'
+                    AND CNORCD = C.CUSOF2)) AS NOMBRE_OFICIAL_SEGUNDO
+                FROM CUMST C
+                WHERE C.CUSOFC IN('244','187')");
+
+        return view('layouts.queries.excel', compact('results'));
+    }
+
+    //Reporte Relación del Cliente como Empleado (Rel. 3)
+    public function reporte_cliente_empleado()
+    {
+        $results = DB::connection('ibs')
+            ->select("SELECT
+                TRIM(CUSCUN) CODIGO_CLIENTE,
+                TRIM(CUSNA1) NOMBRE
+                FROM CUMST C
+                WHERE C.CUSOFC IN('244','187')
+                AND CUSSTF = '3'");
+
+        return view('layouts.queries.excel', compact('results'));
+    }
+
+    //Reporte de tarjeta empleado
+    public function reporte_tdc_empleado()
+    {
+        $results = DB::connection('itc')
+            ->select("SELECT
+                CONCAT(TCACT_MTAR, '*') NUMERO_TARJETA,
+                CONCAT(INDCL_MTAR, '*') IDENTIFICACION,
+                CASE
+                    WHEN CODPR_MTAR = '08' THEN 'DIFERIDO BANDA'
+                    WHEN CODPR_MTAR = '28' THEN 'DIFERIDO CHIP'
+                    WHEN CODPR_MTAR = '11' THEN 'EMPLEADO BANDA'
+                    WHEN CODPR_MTAR = '25' THEN 'EMPLEADO CHIP'
+                END PRODUCTO,
+                NOMPL_MTAR NOMBRE_CLIENTE,
+                NOMBR_DESC ESTATUS,
+                CASE
+                    WHEN MONED_MSAL = '214' THEN 'DOP'
+                    WHEN MONED_MSAL = '840' THEN 'USD'
+                END MONEDA,
+                (SACTC_MSAL + SACTI_MSAL + SACTO_MSAL + SACTG_MSAL) SALDO_ACTUAL,
+                (SACCA_MSAL + SACIN_MSAL + SACCO_MSAL + SACCG_MSAL + SACCB_MSAL + SACIB_MSAL + SACOB_MSAL + SACAG_MSAL) SALDO_CORTE,
+                LIMRD_MSAL LIMITE_CREDITO,
+                CANPV_MSAL CANTIDAD_PAGOS_VENCIDOS,
+                CODCI_MTAR CICLO
+                FROM SATMTAR00
+                INNER JOIN SATMSAL00
+                    ON TCACT_MSAL = TCACT_MTAR
+                INNER JOIN SATDESC00
+                    ON PREFI_DESC = 'SAT_STSTC' AND CODIG_DESC = STSRD_MTAR
+                WHERE CODPR_MTAR = '08'
+                OR CODPR_MTAR = '28'
+                OR CODPR_MTAR = '11'
+                OR CODPR_MTAR = '25'
+                AND STSRD_MTAR NOT IN('09','07','05')");
 
         return view('layouts.queries.excel', compact('results'));
     }
---
+++ resources/views/human_resources/queries/index.blade.php
@@ -0,0 +1,156 @@
+@extends('layouts.master')
+
+@section('title', 'Recursos Humanos -> Vacantes')
+
+@section('page_title', 'Reportes de Recuersos Humanos')
+
+{{-- @if (can_not_do('human_resources_vacant'))
+    @section('contents')
+        @include('layouts.partials.access_denied')
+    @endsection
+@endif --}}
+
+@section('contents')
+
+    <div class="row">
+        <div class="col-xs-8 col-xs-offset-2">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                  <input type="text" id="search_field" placeholder="Busqueda rapida de reporte" class="form-control input-lg" autofocus>
+              </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" id="reports">
+        <div class="col-xs-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-file-excel-o fa-4x"></i>
+                        </div>
+                        <div class="col-xs-10 text-right">
+                            Reporte Cuentas tipo H201/H202
+                        </div>
+                    </div>
+                </div>
+                <a href="{{ route('human_resources.queries.reporte_cuentas') }}" download>
+                    <div class="panel-footer" style="padding: 2px 70px;">
+                        <span class="pull-left">Descargar</span>
+                        <span class="pull-right"><i class="fa fa-arrow-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-file-excel-o fa-4x"></i>
+                        </div>
+                        <div class="col-xs-10 text-right">
+                            Reporte Vinculados por gestión
+                        </div>
+                    </div>
+                </div>
+                <a href="{{ route('human_resources.queries.reporte_vinculados_gestion') }}" download>
+                    <div class="panel-footer" style="padding: 2px 70px;">
+                        <span class="pull-left">Descargar</span>
+                        <span class="pull-right"><i class="fa fa-arrow-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-file-excel-o fa-4x"></i>
+                        </div>
+                        <div class="col-xs-10 text-right">
+                            Reporte Oficial Asignado (Bianca / Victoria)
+                        </div>
+                    </div>
+                </div>
+                <a href="{{ route('human_resources.queries.reporte_oficial_asignado') }}" download>
+                    <div class="panel-footer" style="padding: 2px 70px;">
+                        <span class="pull-left">Descargar</span>
+                        <span class="pull-right"><i class="fa fa-arrow-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-file-excel-o fa-4x"></i>
+                        </div>
+                        <div class="col-xs-10 text-right">
+                            Reporte Relación del Cliente como Empleado
+                        </div>
+                    </div>
+                </div>
+                <a href="{{ route('human_resources.queries.reporte_cliente_empleado') }}" download>
+                    <div class="panel-footer" style="padding: 2px 70px;">
+                        <span class="pull-left">Descargar</span>
+                        <span class="pull-right"><i class="fa fa-arrow-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-file-excel-o fa-4x"></i>
+                        </div>
+                        <div class="col-xs-10 text-right">
+                            Reporte Tarjetas Empleado
+                        </div>
+                    </div>
+                </div>
+                <a href="{{ route('human_resources.queries.reporte_tdc_empleado') }}" download>
+                    <div class="panel-footer" style="padding: 2px 70px;">
+                        <span class="pull-left">Descargar</span>
+                        <span class="pull-right"><i class="fa fa-arrow-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        $('#search_field').keyup(function (e) {
+            var str = $(this).val().trim().toLowerCase();
+
+            if (str != '') {
+                $('#reports .col-xs-3').each(function (index, value) {
+                    var tag = $(value);
+                    var text = $(tag.children('div').children('div').children('div').children('div')[1]).text().trim();
+
+                    if (text.toLowerCase().indexOf(str) >= 0) {
+                        tag.show('slow');
+                    } else {
+                        tag.hide('slow');
+                    }
+                });
+            } else {
+                $('#reports .col-xs-3').each(function (index, value) {
+                    $(value).show('slow');
+                });
+            }
+        });
+    </script>
+
+@endsection
