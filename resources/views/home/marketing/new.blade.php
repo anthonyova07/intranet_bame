f6d145a3e4741915ff3d3053c9f6d4f1360a584b
Jeison Peguero Castro <jpeguerocastro@gmail.com>
2017-03-13 09:53:40 -0400

    cambio de la posicion de los links de las news

--- resources/views/home/marketing/new.blade.php
+++ resources/views/home/marketing/new.blade.php
@@ -13,23 +13,23 @@
 
                 <div class="panel-body">
 
-                    <div class="col-xs-6">
-                        <h3 style="text-align: center;margin-top: 0px;margin-bottom: 16px;">{{ html_entity_decode($new->title) }}</h3>
+                    <div class="col-xs-6 text-center">
+                        <h3 style="margin-top: 0px;margin-bottom: 16px;">{{ html_entity_decode($new->title) }}</h3>
 
                         @if ($new->link_video == '' || !$new->link_video)
                             <img class="img-thumbnail pull-left" src="{{ route('home') . $new->image }}">
                         @else
                             <video autoplay controls class="img-thumbnail video-thumbnail" src="{{ $new->link_video }}"></video>
                         @endif
+
+                        <p>
+                            <a style="font-weight: bold;font-size: 16px;text-decoration: underline;color: #FF8849;" href="{{ $new->link }}" target="__blank">{{ ($new->link_name || $new->link_name <> '' ? $new->link_name : $new->link) }}</a>
+                        </p>
                     </div>
 
                     <div class="col-xs-6">
                         <div style="color: #616365;">
                             {!! html_entity_decode($new->detail) !!}
-
-                            <p>
-                                <a href="{{ $new->link }}" target="__blank">{{ ($new->link_name || $new->link_name <> '' ? $new->link_name : $new->link) }}</a>
-                            </p>
                         </div>
                     </div>
 
