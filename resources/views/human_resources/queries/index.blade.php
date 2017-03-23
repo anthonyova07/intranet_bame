7106b6d93437a6e58f261163673dd2bcb6c5632d
Jeison Peguero Castro <jpeguerocastro@gmail.com>
2017-03-23 17:35:39 -0400

    inico reporte encaje legal

--- resources/views/human_resources/queries/index.blade.php
+++ resources/views/human_resources/queries/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Recursos Humanos -> Vacantes')
 
-@section('page_title', 'Reportes de Recuersos Humanos')
+@section('page_title', 'Reportes de Recursos Humanos')
 
 @if (can_not_do('human_resources_queries'))
     @section('contents')
@@ -152,26 +152,7 @@
     </div>
 
     <script type="text/javascript">
-        $('#search_field').keyup(function (e) {
-            var str = $(this).val().trim().toLowerCase();
-
-            if (str != '') {
-                $('#reports .col-xs-3').each(function (index, value) {
-                    var tag = $(value);
-                    var text = $(tag.children('div').children('div').children('div').children('div')[1]).text().trim();
-
-                    if (text.toLowerCase().indexOf(str) >= 0) {
-                        tag.show('slow');
-                    } else {
-                        tag.hide('slow');
-                    }
-                });
-            } else {
-                $('#reports .col-xs-3').each(function (index, value) {
-                    $(value).show('slow');
-                });
-            }
-        });
+        @include('layouts.functions_js.busqueda_rapida');
     </script>
 
 @endsection
