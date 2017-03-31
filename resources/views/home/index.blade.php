698d6181e908eb9f8192ca73b442e5d03d80c326
Jeison Peguero Castro <jpeguerocastro@gmail.com>
2017-03-31 15:22:46 -0400

    cambios en el panel

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -113,7 +113,7 @@
 
     <div class="row">
 
-        <div class="col-xs-12">
+        <div class="col-xs-9">
             <div class="panel panel-default panel-wiget">
                 <div class="panel-heading">
                     <h3 class="panel-title">Sobre Nosotros</h3>
@@ -159,14 +159,14 @@
                     </div>
                     <div class="col-xs-4">
                         <div class="row">
-                            <div class="well well-sm" style="margin-bottom: 0px;">
+                            <div class="well well-sm" style="margin-bottom: 0px;height: 281px;">
                                 <h3 class="list-group-item-heading text-center" style="color: #d82f27;font-size: 24px;">Código de Ética</h3>
                                 <table class="table table-condensed">
                                     <tbody>
                                         <tr>
-                                            <td class="text-center" data-toggle="tooltip" title="Click para ver">
+                                            <td class="text-center">
                                                 <a target="_blank" href="http://intranet_bame:8077/files/gesti_doc/human_resources/Codigo_de_Etica_y_Conducta/Manual_de_Normas_Codigo_de_Etica_y_Conducta.pdf">
-                                                    <i style="font-size: 183px;color: #d82f27;" class="fa fa-book"></i>
+                                                    <i style="font-size: 183px;color: #d82f27;margin-top: 24px;" data-toggle="tooltip" title="Click para ver" class="fa fa-book"></i>
                                                 </a>
                                             </td>
                                         </tr>
@@ -179,107 +179,23 @@
             </div>
         </div>
 
-    </div>
-
-    <div class="row">
-
-        <div class="col-xs-12" style="margin-top: -18px;margin-bottom: -9px;">
+        <div class="col-xs-3">
             <div class="panel panel-default panel-wiget">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Marco Legal Financiero</h3>
+                    <h3 class="panel-title">Normativa Legal <u data-toggle="tooltip" title="Prevención del Lavado de Activos y el Financiamiento al Terrorismo">PLA/FT</u></h3>
                 </div>
-                <div class="panel-body">
-                    <div class="col-xs-3">
-                        <div class="well well-sm" style="margin-bottom: 0px;">
-                            <h3 class="list-group-item-heading text-center" style="color: #d82f27;font-size: 14px;padding-bottom: 0px;">Circular 004 16 Instructivo Debida Diligencia</h3>
-                            <table class="table table-condensed">
-                                <tbody>
-                                    <tr>
-                                        <td class="text-center" data-toggle="tooltip" title="Click para ver">
-                                            <a target="_blank" href="http://intranet_bame:8077/files/gesti_doc/compliance/Marco_Legal/Circular_004_16_Instructivo_Debida_Diligencia.pdf">
-                                                <i style="font-size: 70px;color: #d82f27;" class="fa fa-gavel"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="col-xs-3">
-                        <div class="well well-sm" style="margin-bottom: 0px;">
-                            <h3 class="list-group-item-heading text-center" style="color: #d82f27;font-size: 14px;padding-bottom: 0px;">Ley Monetaria y Financiera No.183-02</h3>
-                            <table class="table table-condensed">
-                                <tbody>
-                                    <tr>
-                                        <td class="text-center" data-toggle="tooltip" title="Click para ver">
-                                            <a target="_blank" href="http://intranet_bame:8077/files/gesti_doc/compliance/Marco_Legal/Ley_Monetaria-y-Financiera-No.183-02.pdf">
-                                                <i style="font-size: 70px;color: #d82f27;" class="fa fa-gavel"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="col-xs-3">
-                        <div class="well well-sm" style="margin-bottom: 0px;">
-                            <h3 class="list-group-item-heading text-center" style="color: #d82f27;font-size: 14px;padding-bottom: 15px;">Decreto-20-03</h3>
-                            <table class="table table-condensed">
-                                <tbody>
-                                    <tr>
-                                        <td class="text-center" data-toggle="tooltip" title="Click para ver">
-                                            <a target="_blank" href="http://intranet_bame:8077/files/gesti_doc/compliance/Marco_Legal/Decreto-20-03.pdf">
-                                                <i style="font-size: 70px;color: #d82f27;" class="fa fa-gavel"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="col-xs-3">
-                        <div class="well well-sm" style="margin-bottom: 0px;">
-                            <h3 class="list-group-item-heading text-center" style="color: #d82f27;font-size: 14px;padding-bottom: 15px;">Ley 72-02 Lavado Activo</h3>
-                            <table class="table table-condensed">
-                                <tbody>
-                                    <tr>
-                                        <td class="text-center" data-toggle="tooltip" title="Click para ver">
-                                            <a target="_blank" href="http://intranet_bame:8077/files/gesti_doc/compliance/Marco_Legal/Ley_72-02_Lavado_Activo.pdf">
-                                                <i style="font-size: 70px;color: #d82f27;" class="fa fa-gavel"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                <div class="panel-body text-center">
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <div class="well well-sm" style="margin-bottom: 0px;height: 281px;">
+                                {{-- <h3 class="list-group-item-heading text-center" style="color: #d82f27;font-size: 24px;">Normativa Legal PLA/FT</h3> --}}
+                                <a target="_blank" href="http://intranet_bame:8077/gestidoc/compliance?folder=Marco_Legal">
+                                    <i style="font-size: 183px;color: #d82f27;margin-top: 38px;" data-toggle="tooltip" title="Click para ver" class="fa fa-gavel"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-                {{-- <table class="table table-condensed">
-                    <tbody>
-                        <tr>
-                            <td class="text-center" data-toggle="tooltip" title="Circular 004 16 Instructivo Debida Diligencia">
-                                <a target="_blank" href="http://intranet_bame:8077/files/gesti_doc/compliance/Marco_Legal/Circular_004_16_Instructivo_Debida_Diligencia.pdf">
-                                    <i style="font-size: 70px;color: #d82f27;" class="fa fa-book"></i>
-                                </a>
-                            </td>
-                            <td class="text-center" data-toggle="tooltip" title="Ley Monetaria y Financiera No.183-02">
-                                <a target="_blank" href="http://intranet_bame:8077/files/gesti_doc/compliance/Marco_Legal/Ley_Monetaria-y-Financiera-No.183-02.pdf">
-                                    <i style="font-size: 70px;color: #d82f27;" class="fa fa-book"></i>
-                                </a>
-                            </td>
-                            <td class="text-center" data-toggle="tooltip" title="Ley 72-02 Lavado Activo">
-                                <a target="_blank" href="http://intranet_bame:8077/files/gesti_doc/compliance/Marco_Legal/Ley_72-02_Lavado_Activo.pdf">
-                                    <i style="font-size: 70px;color: #d82f27;" class="fa fa-book"></i>
-                                </a>
-                            </td>
-                            <td class="text-center" data-toggle="tooltip" title="Decreto-20-03">
-                                <a target="_blank" href="http://intranet_bame:8077/files/gesti_doc/compliance/Marco_Legal/Decreto-20-03.pdf">
-                                    <i style="font-size: 70px;color: #d82f27;" class="fa fa-book"></i>
-                                </a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table> --}}
             </div>
         </div>
 
@@ -287,7 +203,7 @@
 
     <div class="row">
 
-        <div class="col-xs-4">
+        <div class="col-xs-4" style="margin-top: -8px;">
             <div class="panel panel-default" style="margin-top: 8px;">
 
                 <div class="panel-body text-center">
@@ -352,7 +268,7 @@
             </div>
         </div>
 
-        <div class="col-xs-8">
+        <div class="col-xs-8" style="margin-top: -8px;">
             <div class="panel panel-default panel-wiget" style="display: block;margin-top: 8px;">
                 <div class="panel-heading">
                     <h3 class="panel-title">Calendario Virtual</h3>
@@ -400,7 +316,7 @@
 
     <div class="row">
 
-        <div class="col-xs-4">
+        <div class="col-xs-4" style="margin-top: -15px;">
 
             <div class="panel panel-default panel-wiget">
                 <div class="panel-heading">
@@ -434,7 +350,7 @@
 
         </div>
 
-        <div class="col-xs-4">
+        <div class="col-xs-4" style="margin-top: -15px;">
 
             <div class="panel panel-default panel-wiget">
                 <div class="panel-heading">
