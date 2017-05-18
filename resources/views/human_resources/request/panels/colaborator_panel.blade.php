c754f330886d09c9b982d6f63f4ef961c1549590
Jeison Peguero Castro <jpeguerocastro@gmail.com>
2017-05-18 09:58:46 -0400

    cambio para no tener duplicado el mismo html

--- resources/views/human_resources/request/panels/colaborator_panel.blade.php
+++ resources/views/human_resources/request/panels/colaborator_panel.blade.php
@@ -1,101 +1,79 @@
-@if (isset($human_resource_request))
-    <div class="col-xs-8">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Datos del Empleado</h3>
-            </div>
+<div class="col-xs-8">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Datos del Empleado</h3>
+        </div>
 
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-xs-4">
-                        <div class="form-group{{ $errors->first('coluser') ? ' has-error':'' }}">
-                            <label class="control-label">Usuario</label>
-                            <p class="form-control-static">{{ $human_resource_request->coluser }}</p>
-                            <span class="help-block">{{ $errors->first('coluser') }}</span>
-                        </div>
-                    </div>
-                    <div class="col-xs-4">
-                        <div class="form-group{{ $errors->first('colcode') ? ' has-error':'' }}">
-                            <label class="control-label">Código</label>
-                            <p class="form-control-static">{{ $human_resource_request->colcode }}</p>
-                            <span class="help-block">{{ $errors->first('colcode') }}</span>
-                        </div>
-                    </div>
-                    <div class="col-xs-4">
-                        <div class="form-group{{ $errors->first('colname') ? ' has-error':'' }}">
-                            <label class="control-label">Nombre</label>
-                            <p class="form-control-static">{{ $human_resource_request->colname }}</p>
-                            <span class="help-block">{{ $errors->first('colname') }}</span>
-                        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-xs-4">
+                    <div class="form-group{{ $errors->first('coluser') ? ' has-error':'' }}">
+                        <label class="control-label">Usuario</label>
+                        <p class="form-control-static">
+                            @if (isset($human_resource_request))
+                                {{ $human_resource_request->coluser }}
+                            @else
+                                {{ session()->get('user') }}
+                            @endif
+                            </p>
+                        <span class="help-block">{{ $errors->first('coluser') }}</span>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-xs-6">
-                        <div class="form-group{{ $errors->first('colposi') ? ' has-error':'' }}">
-                            <label class="control-label">Posición</label>
-                            <p class="form-control-static">{{ $human_resource_request->colposi }}</p>
-                            <span class="help-block">{{ $errors->first('colposi') }}</span>
-                        </div>
+                <div class="col-xs-4">
+                    <div class="form-group{{ $errors->first('colcode') ? ' has-error':'' }}">
+                        <label class="control-label">Código</label>
+                        <p class="form-control-static">
+                            @if (isset($human_resource_request))
+                                {{ $human_resource_request->colcode }}
+                            @else
+                                {{ session()->get('user_info')->getPostalCode() }}
+                            @endif
+                        </p>
+                        <span class="help-block">{{ $errors->first('colcode') }}</span>
                     </div>
-                    <div class="col-xs-6">
-                        <div class="form-group{{ $errors->first('coldepart') ? ' has-error':'' }}">
-                            <label class="control-label">Departamento</label>
-                            <p class="form-control-static">{{ $human_resource_request->coldepart }}</p>
-                            <span class="help-block">{{ $errors->first('coldepart') }}</span>
-                        </div>
+                </div>
+                <div class="col-xs-4">
+                    <div class="form-group{{ $errors->first('colname') ? ' has-error':'' }}">
+                        <label class="control-label">Nombre</label>
+                        <p class="form-control-static">
+                            @if (isset($human_resource_request))
+                                {{ $human_resource_request->colname }}
+                            @else
+                                {{ session()->get('user_info')->getFirstName() . ' ' . session()->get('user_info')->getLastName() }}
+                            @endif
+                        </p>
+                        <span class="help-block">{{ $errors->first('colname') }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6">
+                    <div class="form-group{{ $errors->first('colposi') ? ' has-error':'' }}">
+                        <label class="control-label">Posición</label>
+                        <p class="form-control-static">
+                            @if (isset($human_resource_request))
+                                {{ $human_resource_request->colposi }}
+                            @else
+                                {{ session()->get('user_info')->getTitle() }}
+                            @endif
+                        </p>
+                        <span class="help-block">{{ $errors->first('colposi') }}</span>
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <div class="form-group{{ $errors->first('coldepart') ? ' has-error':'' }}">
+                        <label class="control-label">Departamento</label>
+                        <p class="form-control-static">
+                            @if (isset($human_resource_request))
+                                {{ $human_resource_request->coldepart }}
+                            @else
+                                {{ session()->get('user_info')->getDepartment() }}
+                            @endif
+                        </p>
+                        <span class="help-block">{{ $errors->first('coldepart') }}</span>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-@else
-    <div class="col-xs-8">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Datos del Empleado</h3>
-            </div>
-
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-xs-4">
-                        <div class="form-group{{ $errors->first('coluser') ? ' has-error':'' }}">
-                            <label class="control-label">Usuario</label>
-                            <p class="form-control-static">{{ session()->get('user') }}</p>
-                            <span class="help-block">{{ $errors->first('coluser') }}</span>
-                        </div>
-                    </div>
-                    <div class="col-xs-4">
-                        <div class="form-group{{ $errors->first('colcode') ? ' has-error':'' }}">
-                            <label class="control-label">Código</label>
-                            <p class="form-control-static">{{ session()->get('user_info')->getPostalCode() }}</p>
-                            <span class="help-block">{{ $errors->first('colcode') }}</span>
-                        </div>
-                    </div>
-                    <div class="col-xs-4">
-                        <div class="form-group{{ $errors->first('colname') ? ' has-error':'' }}">
-                            <label class="control-label">Nombre</label>
-                            <p class="form-control-static">{{ session()->get('user_info')->getFirstName() . ' ' . session()->get('user_info')->getLastName() }}</p>
-                            <span class="help-block">{{ $errors->first('colname') }}</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-6">
-                        <div class="form-group{{ $errors->first('colposi') ? ' has-error':'' }}">
-                            <label class="control-label">Posición</label>
-                            <p class="form-control-static">{{ session()->get('user_info')->getTitle() }}</p>
-                            <span class="help-block">{{ $errors->first('colposi') }}</span>
-                        </div>
-                    </div>
-                    <div class="col-xs-6">
-                        <div class="form-group{{ $errors->first('coldepart') ? ' has-error':'' }}">
-                            <label class="control-label">Departamento</label>
-                            <p class="form-control-static">{{ session()->get('user_info')->getDepartment() }}</p>
-                            <span class="help-block">{{ $errors->first('coldepart') }}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endif
+</div>
