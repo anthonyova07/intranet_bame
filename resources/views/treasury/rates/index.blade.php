fd384cf3079eb27cbf070df8dbc01f6938648e31
Jeison Peguero Castro <jpeguerocastro@gmail.com>
2017-06-12 12:34:05 -0400

    cambio diseño tasas de tesoreria
    cambio de diseño para la pantall de mantenimiento de las tasas en el
    modulo de tasas de tesoreria

--- resources/views/treasury/rates/index.blade.php
+++ resources/views/treasury/rates/index.blade.php
@@ -13,11 +13,96 @@
 @section('contents')
 
     <div class="row">
+        <div class="col-xs-8 col-xs-offset-2">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                  <a style="font-size: 25px;padding: 15px;font-weight: bold;" class="btn btn-danger btn-xs btn-block" href="{{ route('treasury.rates.create') }}">Nueva Tasa</a>
+              </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" style="border-bottom: 1px solid #777;border-top: 1px solid #777;margin: 8px 0 25px 0;border-width: 5px;">
+        <h1 style="margin: 0;text-align: center;">Mantenimiento de Parametros</h1>
+    </div>
+
+    <div class="row">
+
+        <div class="col-xs-8 col-xs-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Tipos de Productos</h3>
+                </div>
+                <div class="panel-body">
+                    <a class="btn btn-danger btn-xs" href="{{ route('treasury.rates.product.create') }}">Nuevo</a>
+                    <br>
+                    <br>
+                    <table class="table table-striped table-bordered table-hover table-condensed datatable" order-by='0|desc'>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Tipo</th>
+                                <th>Contenido</th>
+                                <th>Activo</th>
+                                <th style="width: 25px"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($products as $product)
+                                <tr>
+                                    <td>{{ $product->name }}</td>
+                                    <td>{{ get_treasury_rate_types($product->rate_type) }}</td>
+                                    <td>{{ get_treasury_rate_contents($product->content) }}</td>
+                                    <td>{{ $product->is_active ? 'Si':'No' }}</td>
+                                    <td align="center">
+                                        <a
+                                            href="{{ route('treasury.rates.product.edit', ['id' => $product->id]) }}"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Editar"
+                                            class="naranja">
+                                            <i class="fa fa-edit fa-fw"></i>
+                                        </a>
+                                        @if (in_array($product->content, ['V', 'R']))
+                                            <a
+                                                href="{{ route('treasury.rates.product.index', array_merge(['product' => $product->id], Request::all())) }}"
+                                                data-toggle="tooltip"
+                                                data-placement="top"
+                                                title="Ver Detalle de {{ $product->name }}">
+                                                <i class="fa fa-share fa-fw"></i>
+                                            </a>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row" style="border-bottom: 1px solid #777;border-top: 1px solid #777;margin: 8px 0 25px 0;border-width: 5px;">
+        <h1 style="margin: 0;text-align: center;">Histórico de Tasas</h1>
+    </div>
+
+    <div class="row">
+
+        <div class="col-xs-6 col-xs-offset-3">
+            <div class="panel panel-default">
+
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
 
         <div class="col-xs-6 col-xs-offset-3">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Búscar Solicitud</h3>
+                    <h3 class="panel-title">Búscar Tasas</h3>
                 </div>
                 <div class="panel-body">
                     <form method="get" action="{{ route('treasury.rates.index') }}" id="form">
@@ -36,47 +121,35 @@
                                     <span class="help-block">{{ $errors->first('date_to') }}</span>
                                 </div>
                             </div>
-                            <div class="col-xs-4">
+                            <div class="col-xs-12">
                                 {{ csrf_field() }}
-                                <button type="submit" class="btn btn-danger btn-xs" id="btn_submit" data-loading-text="Buscando tasas...">Buscar</button>
+                                <button type="submit" class="btn btn-danger btn-xs btn-block" id="btn_submit" data-loading-text="Buscando tasas...">Buscar</button>
                             </div>
                         </div>
                     </form>
                 </div>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="row">
-
-        <div class="col-xs-6 col-xs-offset-3">
-            <div class="panel panel-default">
                 <div class="panel-body">
-                    <a class="btn btn-danger btn-xs" href="{{ route('treasury.rates.create') }}">Nueva Tasa</a>
-                    <br>
-                    <br>
                     <table class="table table-striped table-bordered table-hover table-condensed" order-by='2|desc'>
                         <thead>
                             <tr>
-                                <th>Fecha de la Tasa</th>
-                                <th style="width: 112px;">Fecha Creación</th>
                                 <th style="width: 112px;">Creado por</th>
+                                <th style="width: 112px;">Fecha Creación</th>
+                                {{-- <th>Fecha de la Tasa</th> --}}
                                 <th style="width: 52px"></th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($dates as $date)
                                 <tr>
-                                    <td>{{ $date->effec_date->format('d/m/Y') }}</td>
-                                    <td>{{ $date->created_at->format('d/m/Y H:m:s') }}</td>
                                     <td>{{ $date->createname }}</td>
+                                    <td>{{ $date->created_at->format('d/m/Y H:m:s') }}</td>
+                                    {{-- <td>{{ $date->effec_date->format('d/m/Y') }}</td> --}}
                                     <td align="center">
                                         <a
                                             href="{{ route('treasury.rates.show', array_merge(['date' => $date->id], Request::all())) }}"
                                             data-toggle="tooltip"
                                             data-placement="top"
-                                            title="Ver Tasas">
+                                            title="Ver Tasas Registradas">
                                             <i class="fa fa-share fa-fw"></i>
                                         </a>
                                     </td>
@@ -91,71 +164,6 @@
         </div>
 
     </div>
-
-
-    {{-- @if (!can_not_do('treasury_rates_admin')) --}}
-        <div class="row" style="border-bottom: 1px solid #777;border-top: 1px solid #777;margin: 8px 0 25px 0;border-width: 5px;">
-            <h1 style="margin: 0;text-align: center;">Mantenimientos de Parametros</h1>
-        </div>
-
-        <div class="row">
-
-            <div class="col-xs-6">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Tipos de Productos</h3>
-                    </div>
-                    <div class="panel-body">
-                        <a class="btn btn-danger btn-xs" href="{{ route('treasury.rates.product.create') }}">Nuevo</a>
-                        <br>
-                        <br>
-                        <table class="table table-striped table-bordered table-hover table-condensed datatable" order-by='0|desc'>
-                            <thead>
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Tipo</th>
-                                    <th>Contenido</th>
-                                    <th>Activo</th>
-                                    <th style="width: 25px"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($products as $product)
-                                    <tr>
-                                        <td>{{ $product->name }}</td>
-                                        <td>{{ get_treasury_rate_types($product->rate_type) }}</td>
-                                        <td>{{ get_treasury_rate_contents($product->content) }}</td>
-                                        <td>{{ $product->is_active ? 'Si':'No' }}</td>
-                                        <td align="center">
-                                            <a
-                                                href="{{ route('treasury.rates.product.edit', ['id' => $product->id]) }}"
-                                                data-toggle="tooltip"
-                                                data-placement="top"
-                                                title="Editar"
-                                                class="naranja">
-                                                <i class="fa fa-edit fa-fw"></i>
-                                            </a>
-                                            @if (in_array($product->content, ['V', 'R']))
-                                                <a
-                                                    href="{{ route('treasury.rates.product.index', array_merge(['product' => $product->id], Request::all())) }}"
-                                                    data-toggle="tooltip"
-                                                    data-placement="top"
-                                                    title="Ver Detalle de {{ $product->name }}">
-                                                    <i class="fa fa-share fa-fw"></i>
-                                                </a>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-    {{-- @endif --}}
 
     <script type="text/javascript">
         $('#form').submit(function (vacant) {
