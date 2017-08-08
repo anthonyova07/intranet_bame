db1037371550efd4f80ee1ded485187535d3c3f9
Jeison Peguero Castro <jpeguerocastro@gmail.com>
2017-08-08 16:35:10 -0400

    zoom en la imagen de la noticia
    se ha habilitado la posibilidad de hacer zoom en la imagen de la noticia para cuando esta no se entienda bn

--- resources/views/home/marketing/new.blade.php
+++ resources/views/home/marketing/new.blade.php
@@ -18,9 +18,9 @@
 
                         @if ($new->link_video == '' || !$new->link_video)
                             @if ($new->image == '' || !$new->image)
-                                <img class="img-thumbnail{{ $new->hasDetail() ? ' pull-left' : '' }}" src="{{ route('home') . $new->imgbanner . '?id=' . uniqid() }}">
+                                <img style="cursor: zoom-in;" class="image img-thumbnail{{ $new->hasDetail() ? ' pull-left' : '' }}" src="{{ route('home') . $new->imgbanner . '?id=' . uniqid() }}">
                             @else
-                                <img class="img-thumbnail{{ $new->hasDetail() ? ' pull-left' : '' }}" src="{{ route('home') . $new->image . '?id=' . uniqid() }}">
+                                <img style="cursor: zoom-in;" class="image img-thumbnail{{ $new->hasDetail() ? ' pull-left' : '' }}" src="{{ route('home') . $new->image . '?id=' . uniqid() }}">
                             @endif
                         @else
                             <video autoplay controls class="img-thumbnail video-thumbnail" src="{{ $new->link_video . '?id=' . uniqid() }}"></video>
@@ -44,6 +44,35 @@
             </div>
 
         </div>
+
+        <div class="modal fade">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header" style="padding: 0 0 0 10px;">
+                        <button style="margin: -1px 10px 0 0;font-size: 40px;" type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+                        {{-- <a data-toggle="tooltip" title="Descargar" href="" download style="margin: 8px 10px 0 0;font-size: 27px;font-weight: 700;line-height: 1;text-shadow: 0 1px 0 #fff;" class="pull-right" id="link-download"><i class="fa fa-download" style="color: #d82f27;"></i></a> --}}
+                        <h4>{{ html_entity_decode($new->title) }}</h4>
+                    </div>
+                    <div class="modal-body" style="padding: 0px;">
+                        <div class="row">
+                            <div class="col-xs-12 text-center" style="padding: 0px;">
+                                @if ($new->image == '' || !$new->image)
+                                    <img class="img-thumbnail" src="{{ route('home') . $new->imgbanner . '?id=' . uniqid() }}">
+                                @else
+                                    <img class="img-thumbnail" src="{{ route('home') . $new->image . '?id=' . uniqid() }}">
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script type="text/javascript">
+            $('.image').click(function (event) {
+                $('.modal').modal();
+            });
+        </script>
 
     </div>
 @endsection
