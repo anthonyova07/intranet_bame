efde8129e8c42ccbc13dc024fec7eff25831870e
Jeison Peguero Castro <jpeguerocastro@gmail.com>
2017-09-19 10:15:15 -0400

    cambio label indicado por Mirta Salazar

--- resources/views/pdfs/tdc_receipt.blade.php
+++ resources/views/pdfs/tdc_receipt.blade.php
@@ -23,12 +23,10 @@
                 <tr>
                     <td colspan="2">Nombres</td>
                     <td colspan="2"><b>: {{ $credit_card->getNames() }}</b></td>
-                    <td colspan="2" rowspan="9" align="left">
+                    <td colspan="2" rowspan="9" align="right">
                         <img src="{{ $credit_card->getPhoto() }}" style="width: 150px; height: 140px;">
                         <br>
-                        <div style="text-align: right;">
-                            Autorizo al Banco Múltiple de las Américas S.A. a activar esta tarjeta de crédito en 24 horas después de recibirla.
-                        </div>
+                        Autorizo al Banco Múltiple de las Américas S.A. a activar esta tarjeta de crédito en 24 horas después de recibirla.
                         <br><br><br><br>
                         ______________________________________
                     </td>
